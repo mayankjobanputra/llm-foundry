9537b28bcec6fcada2c163e732d68f1da3950a39
Mayank Jobanputra <mayankjobanputra@gmail.com>
2023-07-09 01:40:27 +0530

    training tokenizer done

---
+++ scripts/bash/train_tokenizers.sh
@@ -0,0 +1,3 @@
+python scripts/train/train_tokenizer.py --json_input_dir ~/Projects/UT/wh_multilingual/data/en/oscar_cleaned/ --tokenizer_output_path ~/Projects/UT/wh_multilingual/models/tokenizers/en_vocab.json --vocab_size 20000
+
+python scripts/train/train_tokenizer.py --json_input_dir ~/Projects/UT/wh_multilingual/data/de/oscar_cleaned/ --tokenizer_output_path ~/Projects/UT/wh_multilingual/models/tokenizers/de_vocab.json --vocab_size 20000
